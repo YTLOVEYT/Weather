3a458f9588368d97c5e7d1ba4ff9dc2821eab94c
yintao <824050253@qq.com>
2017-05-03 13:00:00 +0800

    add the db and drive

---
+++ app/src/main/java/com/example/weather/db/Province.java
@@ -0,0 +1,44 @@
+package com.example.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 省的数据库
+ * Created by Administrator on 2017/5/3.
+ */
+public class Province extends DataSupport
+{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getProvinceName()
+    {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName)
+    {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode()
+    {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode)
+    {
+        this.provinceCode = provinceCode;
+    }
+}
