d9fdbc7c9516dd531ec1ca08275eb779b6725262
yintao <824050253@qq.com>
2017-05-08 21:31:05 +0800

    提交，完成三级列表的功能

---
+++ app/src/main/java/com/example/weather/Const.java
@@ -0,0 +1,12 @@
+package com.example.weather;
+
+/**
+ * Created by Administrator on 2017/5/7.
+ */
+public class Const
+{
+    public static final String MAIN="http://guolin.tech/api";
+    public static final String PROVINCE="/china";
+
+
+}
